205a1d1daa65f959e7cb85c7873f41d93d625e9d
Leandro Gomez <leandro.gz73@gmail.com>
2017-02-10 12:17:17 -0300

    Remuevo los restarts de nginx del container de ckan
    refs:#8

--- ckan-img/roles/andino_server/files/scripts/start_servers.sh
+++ ckan-img/roles/andino_server/files/scripts/start_servers.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -ue
 
-service nginx stop;
-nginx &
 service apache2 restart;
-service nginx restart;
 service redis-server restart;
 service rabbitmq-server restart;
 service supervisor restart;
--- ckan-img/roles/andino_server/files/scripts/theme_f_update.sh
+++ ckan-img/roles/andino_server/files/scripts/theme_f_update.sh
@@ -7,14 +7,10 @@
 #                                                                 #
 ###################################################################
 service apache2 reload;
-service nginx stop;
-nginx &
 while true;
 do
 	while inotifywait -q -e modify /var/lib/ckan/theme_config/settings.json >/dev/null; do
 	    echo "[$(date +%Y-%m-%dT%H:%M:%SZ)] INFO: Cambio en GobAr Theme." > /var/log/ckan/theme.changes.log
 	    service apache2 reload;
-	    service nginx stop;
-	    nginx &
 	done
 done
