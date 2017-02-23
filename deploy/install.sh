4bbcc9395ca609fdb726c0640e6952aa741fcbe1
Leandro Gomez <lgomez@devartis.com>
2017-02-23 12:19:36 -0300

    Fix script de install
    refs:#21

--- deploy/install.sh
+++ deploy/install.sh
@@ -3,7 +3,7 @@ set -e;
 
 sudo mkdir -p /etc/andino/
 
-docker pull datosgobar/portal-andino:development-psql && \
+docker pull datosgobar/portal-andino:development-postgresql && \
         docker pull datosgobar/portal-andino:development-solr && \
         docker pull redis:3.2.7-alpine && \
         docker pull rabbitmq:3.6.6-alpine && \
@@ -11,13 +11,13 @@ docker pull datosgobar/portal-andino:development-psql && \
         docker pull datosgobar/portal-andino:development-andino && \
         docker pull datosgobar/portal-andino:development-nginx;
 
-docker run -d --name andino-psql datosgobar/portal-andino:development-psql && \
+docker run -d --name andino-postgresql datosgobar/portal-andino:development-postgresql && \
     docker run -d --name andino-solr datosgobar/portal-andino:development-solr && \
     docker run -d --name andino-redis redis:3.2.7-alpine && \
     docker run -d --name andino-rabbitmq -h rabbitmq rabbitmq:3.6.6-alpine && \
     docker run -d --name andino-postfix -p 25:25 -p 587:587 catatnight/postfix && \
     docker run -d --name andino -p 8800:8800 \
-        --link andino-psql:db --link andino-solr:solr \
+        --link andino-postgresql:db --link andino-solr:solr \
         --link andino-redis:redis --link andino-rabbitmq:rabbitmq \
         --link andino-postfix:postfix \
         datosgobar/portal-andino:development-andino && \
@@ -28,7 +28,7 @@ docker exec andino /etc/ckan_init.d/init_db.sh
 
 # Run Harvest
 docker run -d --name andino-harvest datosgobar/portal-andino:development-andino \
-        --link andino-psql:db --link andino-solr:solr \
+        --link andino-postgresql:db --link andino-solr:solr \
         --link andino-redis:redis --link andino-rabbitmq:rabbitmq \
         --link andino-postfix:postfix \
         datosgobar/portal-andino:development-andino /etc/ckan_init.d/start_cron.sh
