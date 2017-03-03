b4cf5727b649fca18a2b71c463b231550341fa47
Leandro Gomez <lgomez@devartis.com>
2017-03-03 14:33:36 -0300

    Agrego sleep para evitar problemas con la base de datos
    refs:#6

--- deploy/install.sh
+++ deploy/install.sh
@@ -6,6 +6,8 @@ sudo mkdir -p /etc/andino/
 curl -O https://raw.githubusercontent.com/datosgobar/portal-andino/development/lasted.yml
 docker-compose -f lasted.yml up -d nginx
 
+echo "Wait database for starting up."
+sleep 5;
 # init database
 docker exec andino /etc/ckan_init.d/init_db.sh
 
