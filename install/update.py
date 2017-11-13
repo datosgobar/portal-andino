ee2fd707643671b279a67d481e8fc04b0cfa2ea2
Leandro Gomez <leandro.gz73@gmail.com>
2017-11-13 10:10:32 -0300

    Agrego comentarios en el script de actualizacion

--- install/update.py
+++ install/update.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import argparse
 import subprocess
-from os import path, geteuid, makedirs, remove, symlink, stat, chmod, environ
+from os import path, geteuid, makedirs, remove, symlink, stat, chmod, environ, remove
 from shutil import move
 
 parser = argparse.ArgumentParser(description='Actulizar andino con docker.')
@@ -20,8 +20,6 @@ USR_BIN_CRL = path.join("/usr/local/bin/", CTL_FILE_NAME)
 COMPOSE_FILE_URL = "https://raw.githubusercontent.com/datosgobar/%s/%s/latest.yml" % (REPOSITORY, args.branch)
 CTL_SCRIPT = "https://raw.githubusercontent.com/datosgobar/%s/%s/install/andino-ctl" % (REPOSITORY, args.branch)
 
-EXPECTED_CONTAINERS = ['andino-nginx', 'andino', 'andino-solr', 'andino-postfix', 'andino-redis', 'andino-db']
-
 UPGRADE_DB_COMMAND = "/etc/ckan_init.d/upgrade_db.sh"
 REBUILD_SEARCH_COMMAND = "/etc/ckan_init.d/run_rebuild_search.sh"
 
@@ -29,12 +27,22 @@ directory = args.install_directory
 
 
 def check_permissions():
+    """
+    Verifica el ID del usuario en Linux, debe ser 0 para el usuario root.
+    :return:
+    """
     if geteuid() != 0:
         print("[ ERROR ] Se necesitan permisso de root (sudo).")
         exit(1)
 
 
 def check_installdir(base_path):
+    """
+    Verifica la existencia del directorio de instalación.
+    Si existe, el script termina con error, de lo contrario crea el directorio para instalar la aplicación
+    :param base_path: Directorio de instalación.
+    :return:
+    """
     if path.isdir(base_path):
         print("[ ERROR ] Se encontró instalción previa en %s, aborando." % base_path)
         exit(1)
@@ -51,6 +59,11 @@ def ask(question):
 
 
 def check_docker():
+    """
+    Trata de acceder a docker para verificar que este disponible
+    TODO: No mostrar el output
+    :return:
+    """
     subprocess.check_call([
         "docker",
         "ps"
@@ -58,6 +71,11 @@ def check_docker():
 
 
 def check_compose():
+    """
+    Trata de acceder a docker-compose para verificar que esta disponible
+    TODO: No mostrar el output
+    :return:
+    """
     subprocess.check_call([
         "docker-compose",
         "--version",
@@ -65,13 +83,15 @@ def check_compose():
 
 
 def get_compose_file(base_path):
-    compose_file = "latest.yml"
-    latest_compose_file_path = path.join(base_path, compose_file)
-    if path.isfile(latest_compose_file_path):
-        compose_file_path = latest_compose_file_path
-        print("[ WARN ] Se detecta el archivo %s. Por favor  ")
-    else:
-        compose_file_path = path.join(base_path, "docker-compose.yml")
+    """
+    Descarga el nuevo archivo docker-compose.yml del repositorio.
+
+    El hecho que se llame docker-compose.yml nos permite hacer uso de un archivo "docker-compose.override.yml"
+    en caso de ser necesario. Ver https://docs.docker.com/compose/reference/overview/#specifying-multiple-compose-files
+    :param base_path: Directorio de instalación
+    :return:
+    """
+    compose_file_path = path.join(base_path, "docker-compose.yml")
     subprocess.check_call([
         "curl",
         COMPOSE_FILE_URL,
@@ -82,6 +102,14 @@ def get_compose_file(base_path):
 
 
 def get_ctl_script_file(base_path):
+    """
+    Descarga el archivo para controlar la aplicación "andino-ctl".
+    El mismo provee comandos para controlar y configurar la aplicación.
+
+    Ademas crea un link a `/usr/local/bin/andino-ctl` para que el comando este disponible en el `PATH`
+    :param base_path: Directorio de instalación
+    :return:
+    """
     ctl_script_path = path.join(base_path, CTL_FILE_NAME)
     if path.isfile(ctl_script_path):
         remove(ctl_script_path)
@@ -102,12 +130,26 @@ def get_ctl_script_file(base_path):
 
 
 def call_subprocess(command, directory):
+    """
+    Comando para correr comando con la variable "OVERWRITE_APP_DIR".
+    Esa variable es utilizada en el script andino-ctl para detectar
+    un directorio de instalacion distinto al que se usa por defecto.
+
+    :param command: (list) Comando a correr con subprocess
+    :param directory: directorio de instalación
+    :return:
+    """
     my_env = environ.copy()
     my_env["OVERWRITE_APP_DIR"] = directory
     subprocess.check_call(command, env=my_env)
 
 
 def fix_env_file(base_path):
+    """
+    Lee el archvo `.env` y detecta configuraciones faltantes
+    :param directory: directorio de instalación
+    :return:
+    """
     env_file = ".env"
     env_file_path = path.join(base_path, env_file)
     nginx_var = "NGINX_HOST_PORT"
@@ -129,10 +171,21 @@ def fix_env_file(base_path):
 
 
 def backup(directory):
+    """
+    Corre comandos para crear backups de la base de datos y los archivos de la aplicación
+    :param directory: directorio de instalación
+    :return:
+    """
     call_subprocess([USR_BIN_CRL, "backup_all_files"], directory)
 
 
 def reload_application(directory):
+    """
+    Descarga los contenedores basandose en lo definido en el archivo `docker-compose.yml`.
+    Luego levanta la aplicación con esos contenedores.
+    :param directory: directorio de instalación
+    :return:
+    """
     call_subprocess([
         USR_BIN_CRL,
         "pull",
@@ -145,6 +198,13 @@ def reload_application(directory):
 
 
 def check_previous_installation(base_path):
+    """
+    Verifica que el archivo de docker compose este presente.
+    En case de llamarse "latest.yml" se lo renombra a "docker-compose.yml"
+
+    :param base_path: directorio de instalación
+    :return:
+    """
     latest_file = "latest.yml"
     compose_file = "docker-compose.yml"
 
@@ -156,33 +216,32 @@ def check_previous_installation(base_path):
     if not path.isfile(compose_file_path):
         print("[ ERROR ] No se encontró el archivo %s en el directorio %s" % (compose_file, base_path))
         raise Exception("[ ERROR ] No se encontró una instalación.")
-    output = subprocess.check_output([
-        "docker",
-        "ps",
-        "-a",
-        "--format",
-        "'{{.Names}}'"
-    ]).decode("utf-8")
-    local_containers = [container.strip("'") for container in output.split()]
-    for container_name in EXPECTED_CONTAINERS:
-        if container_name not in local_containers:
-            print("[ ERROR ] El container %s no se encuentra en la aplicación." % container_name)
-            print("[ ERROR ] Por favor instale la aplicación primero.")
-            raise Exception("[ ERROR ] No se encontró una instalación.")
 
 
 def post_update_commands(directory):
+    """
+    Corre los comandos post-update para actualizacion de la aplicación
+    :param directory: directorio de instalación
+    :return:
+    """
     try:
         call_subprocess([
             USR_BIN_CRL,
             "post_update",
         ], directory)
     except subprocess.CalledProcessError as e:
-        print("[ INFO ] Error al correr el script 'andino-ctl post_update'")
+        print("[ ERROR ] Error al correr el script 'andino-ctl post_update'")
         print(e)
+        print("[ INFO ] Continuando")
 
 
 def restart_apps(directory):
+    """
+    Reinicia la aplicación
+
+    :param directory: directorio de instalación
+    :return:
+    """
     call_subprocess([
         USR_BIN_CRL,
         "restart",
