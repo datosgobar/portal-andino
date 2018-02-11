95002c7b0cddf1d8ae8ed7889527a6f59eef0175
Poli García <poli@devartis.com>
2018-02-11 15:58:05 -0300

    Install dev requirements

--- dev.sh
+++ dev.sh
@@ -38,6 +38,7 @@ sub_setup() {
         exit 1;
     fi
     directory="$1"
+    sub_exec /usr/lib/ckan/default/bin/pip install -r "$directory/dev-requirements.txt"
     sub_exec /usr/lib/ckan/default/bin/pip install -e "$directory"
     sub_exec /etc/ckan_init.d/init_dev.sh
     sub_exec /usr/lib/ckan/default/bin/paster serve /etc/ckan/default/production.ini
