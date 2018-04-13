947a6364e1e3d2030443781d01e64ce42af8d3b7
Poli García <poli@devartis.com>
2018-04-13 20:50:13 -0300

    Agrego script para validar el estado de la VPN

---
+++ scripts/test_vpn.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ENVIRONMENT="$1"
+
+echo "Corriendo pruebas de VPN para $ENVIRONMENT";
+
+echo "Inicializando"
+. "$DIR/deploy/variables.sh" "$ENVIRONMENT"
+echo "Setup"
+"$DIR/deploy/prepare.sh"
+
+echo "Pinging target server"
+ping $DEPLOY_TARGET_IP -c 4
+
+echo "Estado del servicio openvpn"
+sudo service openvpn status
+
+echo "Running remote ls command"
+ssh -t $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "ls -lsa"
+
+echo "Running remote command"
+ssh -t $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "echo 'Hello world'"
+
+echo "Running remote command"
+ssh -t $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "whoami && hostname"
