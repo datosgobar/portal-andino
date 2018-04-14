fa8af8eb31fe1e39f9b230e703662335b896ba28
Poli García <poli@devartis.com>
2018-04-13 22:43:37 -0300

    Dejo sólo pruebas de SSH

--- scripts/test_vpn.sh
+++ scripts/test_vpn.sh
@@ -15,34 +15,6 @@ echo "Agregando clave SSH"
 eval "$(ssh-agent -s)"
 ssh-add /tmp/deployment@travis-ci.org
 
-echo "Pinging target server"
-ping $DEPLOY_TARGET_IP -c 4
-
-echo "Estado del servicio openvpn"
-sudo service openvpn status
-
-echo "Chequeo de mtu"
-ifconfig
-
-echo "Busco un valor adecuado de MTU"
-ping -M do -s 1500 -c 1 $DEPLOY_TARGET_IP
-ping -M do -s 1490 -c 1 $DEPLOY_TARGET_IP
-ping -M do -s 1480 -c 1 $DEPLOY_TARGET_IP
-ping -M do -s 1470 -c 1 $DEPLOY_TARGET_IP
-ping -M do -s 1460 -c 1 $DEPLOY_TARGET_IP
-ping -M do -s 1450 -c 1 $DEPLOY_TARGET_IP
-ping -M do -s 1440 -c 1 $DEPLOY_TARGET_IP
-ping -M do -s 1430 -c 1 $DEPLOY_TARGET_IP
-ping -M do -s 1420 -c 1 $DEPLOY_TARGET_IP
-ping -M do -s 1410 -c 1 $DEPLOY_TARGET_IP
-ping -M do -s 1400 -c 1 $DEPLOY_TARGET_IP
-
-echo "Bajo valor de MTU para tun0"
-sudo ifconfig tun0 mtu 1470
-
-echo "Chequeo de mtu"
-ifconfig
-
 echo "Running remote ls command"
 ssh -t $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "ls -lsa"
 
