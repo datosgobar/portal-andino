e64f41a6fc8b7567dc89c9f335d9b3b5ae486840
Poli García <poli@devartis.com>
2018-04-16 13:00:30 -0300

    Deshabilito el upload praa probar más rápido
    Cambio la expresión que evalúa `ENVIRONMENTS`

--- scripts/run_deploy.sh
+++ scripts/run_deploy.sh
@@ -7,11 +7,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TAG="$1"
 ENVIRONMENTS="$2"
 
-"$DIR/upload.sh" "$TAG"
+#"$DIR/upload.sh" "$TAG"
 
 if [ -n "$ENVIRONMENTS" ]; then
-    IFS=';'
-    for env in  "${ENVIRONMENTS[@]}"; do
+    IFS=';';
+    for env in $ENVIRONMENTS; do
         echo "Corriendo Continuous Deployment en $env";
         "$DIR/deploy/run_deploy.sh" "$env"
     done
