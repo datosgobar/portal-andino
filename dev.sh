f6906ebd853ae1788117a65c5990b912b15e562b
Christian Digiorno <cdigiorno@devartis.com>
2018-08-14 14:23:01 -0300

    Hago un chown del archivo que no permite a los workers del datajson funcionar correctamente

--- dev.sh
+++ dev.sh
@@ -75,6 +75,7 @@ sub_setup_with() {
     sub_exec /usr/lib/ckan/default/bin/pip install -r "$directory/dev-requirements.txt"
     sub_exec /usr/lib/ckan/default/bin/pip install -e "$directory"
     sub_exec /etc/ckan_init.d/init_dev.sh
+    chown www-data:www-data /usr/lib/ckan/default/src/ckan/ckan/public/base/i18n/ar.js
     sub_exec supervisorctl restart all
     sub_exec /usr/lib/ckan/default/bin/paster serve /etc/ckan/default/production.ini
 }
