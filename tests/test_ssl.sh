1837f15a6dd7e3ce67e7a1553e7dd3583c8d7e19
Christian Digiorno <cdigiorno@devartis.com>
2018-11-21 14:31:41 -0300

    Corrijo test ssl

--- tests/test_ssl.sh
+++ tests/test_ssl.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e;
 
-if [ return `test "200" != $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "https://localhost:7777"))` ]
+if [ "200" != $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "https://localhost:7777")) ]
 then
     echo "HTTPS Error" ;
     echo $(curl -k -s -o /dev/null -w "%{http_code}" "https://localhost:7777") ;
     exit 1
 fi
 
-if [ return `test "301" != $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "http://localhost")) && "302" != $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "http://localhost"))` ]
+if [[ "301" != $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "http://localhost")) && "302" != $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "http://localhost")) ]]
 then
     echo "HTTP Redirect Error" ;
     echo $(curl -k -s -o /dev/null -w "%{http_code}" "http://localhost") ;
