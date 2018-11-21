10b609224d36da36403f18889b0cc36fe8dcc2cc
Christian Digiorno <cdigiorno@devartis.com>
2018-11-21 12:27:52 -0300

    Creo un archivo de testeo para andino

--- tests/test_ssl.sh
+++ tests/test_ssl.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 set -e;
 
-return `test "200" == $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "https://localhost:7777"))`
-return `test "301" == $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "http://localhost")) || "302" == $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "http://localhost"))`
+if [ return `test "200" != $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "https://localhost:7777"))` ]
+then
+    exit 1
+fi
+
+if [ return `test "301" == $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "http://localhost")) || "302" == $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "http://localhost"))` ]
+then
+    exit 1
+fi
+exit 0
