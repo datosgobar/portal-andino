436435daf23f22940e5825d45ecd8b014373dfd0
Christian Digiorno <cdigiorno@devartis.com>
2018-11-21 13:03:34 -0300

    Actualizo archivo de test de ssl

--- tests/test_ssl.sh
+++ tests/test_ssl.sh
@@ -4,12 +4,14 @@ set -e;
 if [ return `test "200" != $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "https://localhost:7777"))` ]
 then
     echo "HTTPS Error" ;
+    echo curl -k -s -o /dev/null -w "%{http_code}" "https://localhost:7777" ;
     exit 1
 fi
 
 if [ return `test "301" != $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "http://localhost")) && "302" != $(echo $(curl -k -s -o /dev/null -w "%{http_code}" "http://localhost"))` ]
 then
     echo "HTTP Redirect Error" ;
+    echo curl -k -s -o /dev/null -w "%{http_code}" "http://localhost" ;
     exit 1
 fi
 exit 0
