5b95d4f60b18c436a5addc9ca7478cd4685c4bd6
Christian Digiorno <cdigiorno@devartis.com>
2019-01-30 12:34:57 -0300

    Modifico el script de teardown para avisar que no hay containers de Andino corriendo

--- scripts/tear_down_services.sh
+++ scripts/tear_down_services.sh
@@ -4,5 +4,10 @@ if [[ -f /etc/portal/latest.yml ]];
 then
     cd /etc/portal && docker-compose -f /etc/portal/latest.yml down && cd .. && sudo rm -rf /etc/portal/
 else
-    docker kill $(docker ps -q -f name=andino)
+    if [[ $(docker ps -q -f name=andino) ]];
+    then
+        docker kill $(docker ps -q -f name=andino)
+    else
+        echo "No hay contenedores de Andino corriendo."
+    fi
 fi
