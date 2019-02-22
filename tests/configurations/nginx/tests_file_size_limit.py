c94f7e5f2c838c3fdd007229175da680de256b04
Christian Digiorno <cdigiorno@devartis.com>
2019-02-22 10:06:42 -0300

    Revert "Hago un strip del output de subprocess"
    This reverts commit f5f21d78d87be641617a7cb920d0869975175e58.

--- tests/configurations/nginx/tests_file_size_limit.py
+++ tests/configurations/nginx/tests_file_size_limit.py
@@ -12,8 +12,6 @@ class TestFileSizeLimit(TestPortalAndino.TestPortalAndino):
         super(TestFileSizeLimit, cls).setUpClass()
 
     def test_nginx_configuration_uses_1024_MB_as_file_size_limit(self):
-        size_line = subprocess.check_output('docker exec -it andino-nginx cat /etc/nginx/conf.d/default.conf | '
-                                            'grep client_max_body_size', shell=True).strip()
-        print subprocess.check_output('docker exec -it andino-nginx cat /etc/nginx/conf.d/default.conf',
-                                      shell=True).strip()
+        size_line = subprocess.check_output(
+            'docker exec -it andino-nginx cat /etc/nginx/conf.d/default.conf | grep client_max_body_size', shell=True)
         nt.assert_true('1024' in size_line)
