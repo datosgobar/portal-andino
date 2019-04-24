493606b10562b654fd48a90be28ee10900501899
Christian Digiorno <cdigiorno@devartis.com>
2019-04-24 14:38:56 -0300

    Creo funciones para utilización de subprocess

--- install/InstallationManager.py
+++ install/InstallationManager.py
@@ -19,6 +19,12 @@ class InstallationManager:
     def run(self):
         pass
 
+    def run_with_subprocess(self, command):
+        subprocess.check_call(command, shell=True)
+
+    def get_subprocess_output(self, command):
+        return subprocess.check_output(command, shell=True).strip()
+
     def parse_args(self):
         pass
 
@@ -28,28 +34,16 @@ class InstallationManager:
             exit(1)
 
     def check_docker(self):
-        subprocess.check_call([
-            "docker",
-            "ps"
-        ])
+        self.run_with_subprocess("docker ps")
 
     def check_compose(self):
-        subprocess.check_call([
-            "docker-compose",
-            "--version",
-        ])
+        self.run_with_subprocess("docker-compose --version")
 
     def download_file(self, file_path, download_url):
-        subprocess.check_call([
-            "curl",
-            download_url,
-            "--fail",
-            "--output",
-            file_path
-        ])
+        self.run_with_subprocess("curl {0} --fail --output {1}".format(download_url, file_path))
 
     def get_compose_file(self, base_path, download_url, compose_file, use_local_compose_files):
-        parent_directory = os.path.abspath(os.path.join(subprocess.check_output('pwd', shell=True).strip(), os.pardir))
+        parent_directory = os.path.abspath(os.path.join(self.get_subprocess_output('pwd'), os.pardir))
         local_compose_file_path = path.join(parent_directory, compose_file)
         dest_compose_file_path = path.join(base_path, compose_file)
         if use_local_compose_files and os.path.isfile(local_compose_file_path):
@@ -92,90 +86,51 @@ class InstallationManager:
 
     def pull_application(self, compose_path, dev_compose_path, theme_volume_src):
         extra_file_argument = "-f {}".format(dev_compose_path) if theme_volume_src else ""
-        subprocess.check_call(
-            ["docker-compose -f {0} {1} pull --ignore-pull-failures".format(compose_path, extra_file_argument)],
-            shell=True)
+        self.run_with_subprocess(
+            "docker-compose -f {0} {1} pull --ignore-pull-failures".format(compose_path, extra_file_argument))
 
     def load_application(self, compose_path, dev_compose_path, theme_volume_src):
         extra_file_argument = "-f {}".format(dev_compose_path) if theme_volume_src else ""
-        subprocess.check_call(["docker-compose -f {0} {1} up -d nginx".format(compose_path, extra_file_argument)],
-                              shell=True)
+        self.run_with_subprocess(
+            "docker-compose -f {0} {1} up -d nginx".format(compose_path, extra_file_argument))
 
     def configure_application(self, compose_path):
         pass
 
     def restart_apps(self, compose_path):
-        subprocess.check_call([
-            "docker-compose",
-            "-f",
-            compose_path,
-            "restart",
-        ])
+        self.run_with_subprocess("docker compose -f {} restart".format(compose_path))
 
     def configure_nginx_extended_cache(self, compose_path):
-        subprocess.check_call([
-            "docker-compose",
-            "-f",
-            compose_path,
-            "exec",
-            "-T",
-            "portal",
-            "/etc/ckan_init.d/update_conf.sh",
-            "andino.cache_clean_hook=http://nginx/meta/cache/purge",
-        ])
-        subprocess.check_call([
-            "docker-compose",
-            "-f",
-            compose_path,
-            "exec",
-            "-T",
-            "portal",
-            "/etc/ckan_init.d/update_conf.sh",
-            "andino.cache_clean_hook_method=PURGE",
-        ])
+        self.run_with_subprocess(
+            "docker-compose -f {} exec -T portal /etc/ckan_init.d/update_conf.sh "  # TODO: usar update_config_file_value
+            "andino.cache_clean_hook=http://nginx/meta/cache/purge".format(compose_path))
+        self.run_with_subprocess(
+            "docker-compose -f {} exec -T portal /etc/ckan_init.d/update_conf.sh "
+            "andino.cache_clean_hook_method=PURGE".format(compose_path))
 
     def include_necessary_nginx_configuration(self, filename):
-        subprocess.check_call([
-            "docker",
-            "exec",
-            "-d",
-            "andino-nginx",
-            "/etc/nginx/scripts/{0}".format(filename)
-        ])
+        self.run_with_subprocess("docker exec -d andino-nginx /etc/nginx/scripts/{}".format(filename))
 
     def persist_ssl_certificates(self, cfg):
-        subprocess.check_call([
-            "docker",
-            "cp",
-            cfg.ssl_key_path,
-            'andino-nginx:{0}/andino.key'.format(self.nginx_ssl_config_directory)
-        ])
-        subprocess.check_call([
-            "docker",
-            "cp",
-            cfg.ssl_crt_path,
-            'andino-nginx:{0}/andino.crt'.format(self.nginx_ssl_config_directory)
-        ])
+        self.copy_file_to_container(
+            cfg.ssl_key_path, "andino-nginx:{}/andino.key".format(self.nginx_ssl_config_directory))
+        self.copy_file_to_container(
+            cfg.ssl_key_path, "andino-nginx:{}/andino.crt".format(self.nginx_ssl_config_directory))
+
+    def copy_file_to_container(self, src, dst):
+        self.run_with_subprocess("docker cp {0} {1}".format(src, dst))
 
     def update_config_file_value(self, value, compose_path):
         if value:
-            subprocess.check_call([
-                "docker-compose",
-                "-f",
-                compose_path,
-                "exec",
-                "-T",
-                "portal",
-                "/etc/ckan_init.d/update_conf.sh",
-                value,
-            ])
+            self.run_with_subprocess(
+                "docker-compose -f {0} exec -T portal /etc/ckan_init.d/update_conf.sh {1}".format(compose_path, value))
 
     def ping_nginx_until_200_response_or_timeout(self, site_url):
         timeout = time.time() + 60 * 5  # límite de 5 minutos
         site_status_code = 0
         while site_status_code != "200":
-            site_status_code = subprocess.check_output(
-                'echo $(curl -k -s -o /dev/null -w "%{{http_code}}" {})'.format(site_url), shell=True).strip()
+            site_status_code = self.get_subprocess_output(
+                'echo $(curl -k -s -o /dev/null -w "%{{http_code}}" {})'.format(site_url))
             print("Intentando comunicarse con: {0} - Código de respuesta: {1}".format(site_url, site_status_code))
             if time.time() > timeout:
                 self.logger.warning("No fue posible reiniciar el contenedor de Nginx. "
