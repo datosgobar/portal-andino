7577227138a67fdcfedfa262c00f1245735da4bd
Christian Digiorno <cdigiorno@devartis.com>
2019-05-03 15:20:27 -0300

    Fix deploy

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -12,7 +12,7 @@ ssh-add /tmp/deployment@travis-ci.org
 echo "Ejecutando comando de instalación..."
 
 # Actualizo el script de actualización
-ssh $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "rm -rf ~/update.py"
-scp -P $DEPLOY_TARGET_SSH_PORT "install/update.py" "$DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP:~/update.py"
+ssh $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "rm -rf ~/update.py ~/installation_manager.py"
+scp -P $DEPLOY_TARGET_SSH_PORT "install/update.py install/installation_manager.py" "$DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP:~/"
 
 ssh -t $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "sudo python ~/update.py --andino_version $DEPLOY_TAG $DEPLOY_SSL_ENABLE_FLAG --ssl_crt_path $DEPLOY_SSL_CRT_PATH --ssl_key_path $DEPLOY_SSL_KEY_PATH"
