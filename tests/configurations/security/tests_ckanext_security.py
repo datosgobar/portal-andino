796727cfc6b2a4343bd5777e0c78ffb80a7fe8e4
Christian Digiorno <cdigiorno@devartis.com>
2019-05-28 09:50:11 -0300

    Fix test

--- tests/configurations/security/tests_ckanext_security.py
+++ tests/configurations/security/tests_ckanext_security.py
@@ -7,23 +7,21 @@ from tests import TestPortalAndino
 
 class TestCkanextSecurity(TestPortalAndino.TestPortalAndino):
 
-    def __init__(self):
-        super(TestCkanextSecurity, self).__init__()
-        self.security_path = "/etc/ckan_init.d/security/"
-        self.enablement_script = "enable_ckanext_security.sh"
-        self.disablement_script = "disable_ckanext_security.sh"
-
     def test_successful_plugin_enablement_and_disablement(self):
+        security_path = "/etc/ckan_init.d/security/"
+        enablement_script = "enable_ckanext_security.sh"
+        disablement_script = "disable_ckanext_security.sh"
+
         # Enable
         subprocess.check_call(
-            "docker exec -it andino {0}./{1}".format(self.security_path, self.enablement_script), shell=True)
+            "docker exec -it andino {0}./{1}".format(security_path, enablement_script), shell=True)
         cmd = "grep 'ckan.site_url = https://localhost' /etc/ckan/default/production.ini"
         search_result = subprocess.check_output("docker exec -it andino {}".format(cmd)).strip()
         nt.assert_equal("## ckanext-security", search_result)
 
         # Disable
         subprocess.check_call(
-            "docker exec -it andino {0}./{1}".format(self.security_path, self.disablement_script), shell=True)
+            "docker exec -it andino {0}./{1}".format(security_path, disablement_script), shell=True)
         cmd = "{} || true".format(cmd)
         search_result = subprocess.check_output("docker exec -it andino {}".format(cmd)).strip()
         nt.assert_equal("", search_result)
