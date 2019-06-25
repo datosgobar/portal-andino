37054481b7f1d1590e2fc7a530761ba54b14babb
Christian Digiorno <cdigiorno@devartis.com>
2019-06-25 12:51:03 -0300

    Utilizo certificados dentro del server, no en travis

--- scripts/deploy/deploy.sh
+++ scripts/deploy/deploy.sh
@@ -21,7 +21,11 @@ scp -P $DEPLOY_TARGET_SSH_PORT "install/installation_manager.py" "$DEPLOY_TARGET
 if [[ $ENVIRONMENT = 'andino-dev' ]] ; then dev_domain='portal-andino-v3d.datos.gob.ar' ; elif [[ $ENVIRONMENT = 'datosgobar-dev' ]] ; then dev_domain='v3d.datos.gob.ar' ; fi
 if [ -z $dev_domain ] ; then
     echo "Generando certificados para $dev_domain";
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/CN=$dev_domain/O=My Company Name LTD./C=AR" -keyout $DEPLOY_SSL_KEY_PATH -out $DEPLOY_SSL_CRT_PATH
+    dev_certs_dir="/tmp/dev_certs"
+    DEPLOY_SSL_CRT_PATH="$dev_certs_dir/andino.crt"
+    DEPLOY_SSL_KEY_PATH="$dev_certs_dir/andino.key"
+    ssh -t $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "mkdir $dev_certs_dir"
+    ssh -t $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj \"/CN=$dev_domain/O=My Company Name LTD./C=AR\" -keyout $DEPLOY_SSL_KEY_PATH -out $DEPLOY_SSL_CRT_PATH"
 fi
 
 ssh -t $DEPLOY_TARGET_USERNAME@$DEPLOY_TARGET_IP -p$DEPLOY_TARGET_SSH_PORT "sudo python ~/update.py --andino_version $DEPLOY_TAG $DEPLOY_SSL_ENABLE_FLAG --ssl_crt_path $DEPLOY_SSL_CRT_PATH --ssl_key_path $DEPLOY_SSL_KEY_PATH"
